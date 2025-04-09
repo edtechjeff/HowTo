136ebb71a0b19bc0ea0bbb8e304d870b2ada55a4
edtech.jeff <edtech.jeff@gmail.com>
2025-04-09 17:28:59 -0400

    commit

--- Scripts/Intune/Endpoints/AVServiceCheck.ps1
+++ Scripts/Intune/Endpoints/AVServiceCheck.ps1
@@ -1,3 +1,5 @@
+# This script is used to check the status of Windows Defender on a system.
+# It checks if the Defender services are running and if real-time protection is enabled.
 (Get-MpComputerStatus).AMServiceEnabled
 (Get-MpComputerStatus).AntispywareEnabled
 (Get-MpComputerStatus).AntivirusEnabled
